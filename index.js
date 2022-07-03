6de440374b75e182a793ea29a3023d4075c08dbe
Subhashis Hansda <subhashishansda2@gmail.com>
2022-07-03 20:10:53 +0530

    added audio for the keys of the drum

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert("hello")
+var drum_buttons = document.querySelectorAll(".drum").length;
+
+var i=0;
+while (i < drum_buttons) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var audio = new Audio("assets/sounds/kick-bass.mp3");
+        audio.play();
+    });
+    i++;
+}
